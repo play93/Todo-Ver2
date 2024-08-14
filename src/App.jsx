76712a6db8c336e13147a4cf8df7d6b24b73dcd1
play93 <95739833+play93@users.noreply.github.com>
2024-08-15 01:07:57 +0900

    기능 구현 준비하기 완

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 import "./App.css";
-import { useState } from "react";
+import { useState } from "react"; //useState라는 리액트 훅을 불러옴
 import Header from "./components/Header";
 import Editor from "./components/Editor";
 import List from "./components/List";
 
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "산책하기",
+    date: new Date().getTime(),
+  },
+];
+
 function App() {
+  const [todos, setTodos] = useState(mockData);
+
   return (
     <div className="App">
       <Header />
