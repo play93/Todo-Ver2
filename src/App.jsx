fa52e81ec1627498f870116f83afe2711ef3807a
play93 <95739833+play93@users.noreply.github.com>
2024-08-15 12:57:21 +0900

    todo를 추가해 state에 저장

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState } from "react"; //useState라는 리액트 훅을 불러옴
+import { useState, useRef } from "react"; //useState라는 리액트 훅을 불러옴
 import Header from "./components/Header";
 import Editor from "./components/Editor";
 import List from "./components/List";
@@ -28,10 +28,23 @@ const mockData = [
 function App() {
   const [todos, setTodos] = useState(mockData);
 
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime(),
+    };
+
+    setTodos([newTodo, ...todos]);
+  };
+
   return (
     <div className="App">
       <Header />
-      <Editor />
+      <Editor onCreate={onCreate} />
       <List />
     </div>
   );
